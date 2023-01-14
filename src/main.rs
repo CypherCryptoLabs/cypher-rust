2ae89b7d6ea21b0072b7fd8ea03bd9e2ddc7303f
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2023-01-14 22:28:42 +0100

    add default route, leading to a custom 404 page

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ extern crate hyper_router;
 extern crate serde;
 extern crate serde_json;
 
-use std::array;
-
 use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
-use hyper::{Request, Response, Body, Method};
+use hyper::{Request, Response, Body, StatusCode};
 use hyper::server::Server;
 use hyper::rt::Future;
 use hyper_router::{Route, RouterBuilder, RouterService};
@@ -43,9 +41,15 @@ fn router_service() -> Result<RouterService, std::io::Error> {
     let router = RouterBuilder::new()
         .add(Route::get("/version").using(request_handler_version))
         .add(Route::get(&("/v".to_owned() + _NODE_VERSION + "/nodes")).using(request_handler_get_nodes))
+        .add(Route::get("*").using(|_req| {
+            let custom_404 = "404 - Route not supported by this Node.";
+            let mut response = Response::new(Body::from(custom_404));
+            *response.status_mut() = StatusCode::NOT_FOUND;
+            response
+        }))
         .build();
 
-    Ok(RouterService::new(router))
+        Ok(RouterService::new(router))
 }
 
 fn main() {
