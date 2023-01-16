a74d9bc715d9546557979582c6b61be619aba35b
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2023-01-16 23:30:50 +0100

    upgrade hyper version

--- src/networking/mod.rs
+++ src/networking/mod.rs
@@ -1,21 +1,46 @@
 extern crate hyper;
-extern crate hyper_router;
+extern crate hyper_routing;
 extern crate serde;
 extern crate serde_json;
 
 mod route_handler;
 
-use hyper::server::Server;
-use hyper::rt::Future;
+use std::task::{Context, Poll};
+use futures_util::future;
+use hyper::service::Service;
+use hyper_routing::{RouterService};
+use tokio;
 
 const _NODE_VERSION: &str = env!("CARGO_PKG_VERSION");
 const _NODE_NAME: &str = env!("CARGO_PKG_NAME");
 
-pub fn start_http_server() {
-    let addr = "0.0.0.0:1234".parse().unwrap();
-    let server = Server::bind(&addr)
-        .serve(route_handler::router_service)
-        .map_err(|e| eprintln!("server error: {}", e));
+pub struct MakeSvc;
 
-    hyper::rt::run(server)
+impl<T> Service<T> for MakeSvc {
+  type Response = RouterService;
+  type Error = std::io::Error;
+  type Future = future::Ready<Result<Self::Response, Self::Error>>;
+
+  fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+    Ok(()).into()
+  }
+
+  fn call(&mut self, _: T) -> Self::Future {
+    future::ok(route_handler::router_service())
+  }
+}
+
+
+#[tokio::main]
+pub async fn start_http_server() {
+
+    // We'll bind to 127.0.0.1:3000
+    let addr = "0.0.0.0:1234".parse().unwrap();
+
+    let server = hyper::Server::bind(&addr).serve(MakeSvc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
--- src/networking/route_handler/mod.rs
+++ src/networking/route_handler/mod.rs
@@ -1,20 +1,21 @@
 extern crate hyper;
-extern crate hyper_router;
+extern crate hyper_routing;
 extern crate serde;
 extern crate serde_json;
 
 use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
 use hyper::{Request, Response, Body, StatusCode};
-use hyper_router::{Route, RouterBuilder, RouterService};
+use hyper_routing::{Route, RouterBuilder, RouterService};
 use serde_json::json;
 
 static _NODE_VERSION: &str = env!("CARGO_PKG_VERSION");
 static _NODE_NAME: &str = env!("CARGO_PKG_NAME");
 
-pub fn router_service() -> Result<RouterService, std::io::Error> {
+pub fn router_service() -> RouterService {
     let router = RouterBuilder::new()
         .add(Route::get("/version").using(get_version))
         .add(Route::get(&("/v".to_owned() + _NODE_VERSION + "/network")).using(get_nodes))
+        .add(Route::post(&("/v".to_owned() + _NODE_VERSION + "/network/node")).using(post_node))
         .add(Route::get("*").using(|_req| {
             let custom_404 = "404 - Route not supported by this Node.";
             let mut response = Response::new(Body::from(custom_404));
@@ -23,7 +24,7 @@ pub fn router_service() -> Result<RouterService, std::io::Error> {
         }))
         .build();
 
-        Ok(RouterService::new(router))
+    RouterService::new(router)
 }
 
 fn get_version(_: Request<Body>) -> Response<Body> {
@@ -51,4 +52,15 @@ fn get_nodes(_: Request<Body>) -> Response<Body> {
             .body(Body::from(body))
             .expect("Failed to construct the response")
     }
+}
+
+fn post_node(_: Request<Body>) -> Response<Body> {
+
+    let body: String = "".to_string();
+
+    Response::builder()
+            .header(CONTENT_LENGTH, body.len() as u64)
+            .header(CONTENT_TYPE, "text/plain")
+            .body(Body::from(body))
+            .expect("Failed to construct the response")
 }
