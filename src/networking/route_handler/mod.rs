5bb618c5d16f2202e7406c10b7d0b7cdb51cae87
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2023-01-20 20:35:52 +0100

    make request body data usable

--- src/networking/route_handler/mod.rs
+++ src/networking/route_handler/mod.rs
@@ -4,78 +4,73 @@ extern crate serde;
 extern crate serde_json;
 
 use std::convert::Infallible;
-
-use futures_util::{TryStreamExt};
+use std::pin::Pin;
+use futures_util::{ Future};
 use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
-use hyper::{Request, Response, Body, StatusCode};
+use hyper::{Request, Response, Body, StatusCode, body};
 use serde_json::json;
+
+impl Copy for Request<Body> {}
+trait Copy {}
+
+struct Route<'a> {
+    path: &'a str,
+    method: &'a str,
+    handler:  fn(Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send>>
+}
 
 static _NODE_VERSION: &str = env!("CARGO_PKG_VERSION");
 static _NODE_NAME: &str = env!("CARGO_PKG_NAME");
-static mut API_ENDPOINTS: Vec<(&str, Box<dyn Fn(Request<Body>) -> Response<Body>>, &str)> = vec![
-    (&("/version"), unsafe {std::mem::transmute(&get_version)}, "GET"),
-    (&("/v".to_owned() + _NODE_VERSION + "/network"), unsafe {std::mem::transmute(&get_nodes)}, "GET"),
-    (&("/v".to_owned() + _NODE_VERSION + "/network/node"), unsafe {std::mem::transmute(&post_node)}, "POST"),
-];
-
-// pub fn router_service() -> RouterService {
-//     let router = RouterBuilder::new()
-//         .add(Route::get("/version").using(get_version))
-//         .add(Route::get(&("/v".to_owned() + _NODE_VERSION + "/network")).using(get_nodes))
-//         .add(Route::post(&("/v".to_owned() + _NODE_VERSION + "/network/node")).using(post_node))
-//         .add(Route::get("*").using(|_req| {
-//             let custom_404 = "404 - Route not supported by this Node.";
-//             let mut response = Response::new(Body::from(custom_404));
-//             *response.status_mut() = StatusCode::NOT_FOUND;
-//             response
-//         }))
-//         .build();
-
-//     RouterService::new(router)
-// }
 
 pub async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+
+    //let request_borrowed: &Request<Body> = &req;
     let request_path: &str = req.uri().path();
-    let request_method = req.method().to_string();
-    let request_method = request_method.as_str();
-    let mut response = Response::new(Body::from("404 - Route not supported by this Node."));
-    *response.status_mut() = StatusCode::NOT_FOUND;
+    let request_method: String = req.method().to_string();
+    let request_method: &str = request_method.as_str();
+    let mut response: Option<Response<Body>> = None;
 
-    unsafe {
-        let api_endpoints_ref = &API_ENDPOINTS;
-        for (request_path, request_handler, request_method) in api_endpoints_ref.into_iter() {
+    let get_nodes_path = "/v".to_owned() + _NODE_VERSION + "/network";
+    let post_nodes_path = "/v".to_owned() + _NODE_VERSION + "/network/node";
 
-            if request_path == request_path && request_method == request_method {
-                response = request_handler(req);
+    let api_routes: Vec<Route> = vec![
+        Route {path: &"/version", method: &"GET", handler: get_version},
+        Route {path: &get_nodes_path, method: &"GET", handler: get_nodes},
+        Route {path: &post_nodes_path, method: &"POST", handler: post_node},
+    ];
+
+    for route in api_routes.iter() {
+        if request_path == route.path &&
+            request_method == route.method {
+                //let request_static= Box::leak(Box::new(req));
+                response = Some((route.handler)(Request::new(req.into_body())).await);
                 break;
             }
-        }
     }
 
-    Ok(response)
+    if response.is_none() {
+        let body: String = "404 - Route not supported by this Node.".to_string();
+        let mut error_response: Response<Body> = Response::builder()
+                .header(CONTENT_LENGTH, body.len() as u64)
+                .header(CONTENT_TYPE, "text/plain")
+                .body(Body::from(body))
+                .expect("Failed to construct the response");
+
+            *error_response.status_mut() = StatusCode::NOT_FOUND;
+
+        response = Some(error_response)
+    }
+
+    Ok(response.unwrap())
 
 }
 
-async fn get_version(_: Request<Body>) -> Response<Body> {
-    
-    let body: String = serde_json::to_string(&serde_json::json!({
-        "node_name": _NODE_NAME,
-        "node_version": _NODE_VERSION
-    })).unwrap();
-
-    let response = Response::builder()
-        .header(CONTENT_LENGTH, body.len() as u64)
-        .header(CONTENT_TYPE, "text/plain")
-        .body(Body::from(body))
-        .expect("Failed to construct the response");
-
-    response
-}
-
-async fn get_nodes(_: Request<Body>) -> Response<Body> {
-    
-    unsafe {
-        let body: String = serde_json::to_string(&json!(&super::super::consensus::NODE_LIST)).unwrap();
+fn get_version(_req: Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send>> {
+    Box::pin(async {
+        let body: String = serde_json::to_string(&serde_json::json!({
+            "node_name": _NODE_NAME,
+            "node_version": _NODE_VERSION
+        })).unwrap();
 
         let response = Response::builder()
             .header(CONTENT_LENGTH, body.len() as u64)
@@ -84,40 +79,60 @@ async fn get_nodes(_: Request<Body>) -> Response<Body> {
             .expect("Failed to construct the response");
 
         response
-    }
+    })
 }
 
+fn get_nodes(_req: Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send>> {
+    Box::pin(async move {
+        unsafe {
+            let body: String = serde_json::to_string(&json!(&super::super::consensus::NODE_LIST)).unwrap();
 
-async fn post_node(req: Request<Body>) -> Response<Body> {
-    
-    let request_body_bytes = req.into_body().try_next().await.unwrap().unwrap();
-    let data = String::from_utf8(request_body_bytes.to_vec());
-    
-    let mut response: Response<Body>;
-
-    match data {
-        Ok(_) => {
-            println!("{:#?}", data);
-            let body = data.unwrap();
-            response = Response::builder()
+            let response = Response::builder()
                 .header(CONTENT_LENGTH, body.len() as u64)
                 .header(CONTENT_TYPE, "text/plain")
                 .body(Body::from(body))
-                .expect("Failed to construct the response")
+                .expect("Failed to construct the response");
+
+            response
         }
-        Err(_) => {
-            let body = "404 - Route not supported by this Node.".to_string();
-            response = Response::builder()
+    })
+}
+
+fn post_node(req: Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send>> {
+    Box::pin(async move {
+        // let req_clone = req.to_owned().clone();
+        // let request_body_bytes = req_clone.into_body().filter_map(|b| async {b.ok()}).collect::<Vec<hyper::body::Bytes>>().await;
+        // let request_body_bytes_vec: Vec<u8> = request_body_bytes.into_iter().flat_map(|b| b.to_vec()).collect();
+        // let data = String::from_utf8(request_body_bytes_vec);
+
+        let body = req.into_body();
+        let bytes = body::to_bytes(body).await.unwrap();
+        let data = String::from_utf8((&*bytes).to_vec());
+
+        let mut response: Response<Body>;
+
+        match data {
+            Ok(_) => {
+                println!("{:#?}", data);
+                let body = data.unwrap();
+                response = Response::builder()
                     .header(CONTENT_LENGTH, body.len() as u64)
                     .header(CONTENT_TYPE, "text/plain")
                     .body(Body::from(body))
-                    .expect("Failed to construct the response");
+                    .expect("Failed to construct the response")
+            }
+            Err(_) => {
+                let body = "404 - Route not supported by Node";
+                response = Response::builder()
+                        .header(CONTENT_LENGTH, body.len() as u64)
+                        .header(CONTENT_TYPE, "text/plain")
+                        .body(Body::from(body))
+                        .expect("Failed to construct the response");
 
-            *response.status_mut() = StatusCode::BAD_REQUEST;
-        },
-    }
+                *response.status_mut() = StatusCode::BAD_REQUEST;
+            },
+        }
 
-    response
-
-
+        response
+    })
 }
