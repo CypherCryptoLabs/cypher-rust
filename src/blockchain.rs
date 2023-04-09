a8c275829e5bceec3dd9466e00f8b8debc089ae3
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2023-04-09 17:22:35 +0200

    generate Block for each voting slot

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,3 +1,8 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+use crate::networking::node::LOCAL_BLOCKCHAIN_ADDRESS;
+
+use super::networking::node;
+
 extern crate serde;
 extern crate serde_json;
 extern crate rand;
@@ -19,4 +24,32 @@ pub struct Block {
     pub payload: Vec<Tx>,
     pub forger_signature: String,
     pub validators: Vec<(String, String)> 
+}
+
+impl Block {
+    pub fn new(tx: Vec<Tx>) -> Block {
+        let mut temp_block =  Block {
+            timestamp: SystemTime::now().duration_since(UNIX_EPOCH)
+            .expect("Time went backwards").as_millis() as u64,
+            forger: LOCAL_BLOCKCHAIN_ADDRESS.to_string(),
+            payload: tx,
+            forger_signature: "".to_string(),
+            validators: vec![],
+        };
+
+        let block_json = match serde_json::to_string(&temp_block.clone()) {
+            Ok(json) => {
+                json
+            },
+            Err(e) => {
+                println_debug!("{}", e);
+                temp_block.timestamp = 0;
+                "".to_string()
+            }
+        };
+
+        temp_block.forger_signature = super::crypto::sign_string(&block_json);
+        return temp_block;
+        
+    }
 }
--- src/queue_worker.rs
+++ src/queue_worker.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::time::{SystemTime, UNIX_EPOCH};
-use crate::blockchain::Tx;
+use crate::blockchain;
 
 use super::transaction_queue;
 
@@ -14,14 +14,21 @@ pub fn init() {
 
             thread::sleep(Duration::from_millis(sleep_timer));
 
+            let transactions_for_block: Vec<blockchain::Tx>;
             unsafe { 
-                let mut transactions: Vec<Tx> = transaction_queue::TX_HASHMAP.as_mut().unwrap().values().cloned().collect();
+                let mut transactions: Vec<blockchain::Tx> = transaction_queue::TX_HASHMAP.as_mut().unwrap().values().cloned().collect();
                 transactions.sort_unstable_by(|a, b| b.network_fee.cmp(&a.network_fee));
-                let transactions_for_block: Vec<Tx> = transactions.into_iter().take(100).collect();
+                transactions_for_block= transactions.into_iter().take(100).collect();
                 println_debug!("{:#?}", transactions_for_block);
             };
 
-            println_debug!("{}", SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis() as u64);
+            let block = blockchain::Block::new(transactions_for_block);
+            if block.timestamp == 0 {
+                println_debug!("Block could not be created, skipping current iteration!");
+                continue;
+            }
+
+            println_debug!("{:#?}", block);
         }
     });
 }
